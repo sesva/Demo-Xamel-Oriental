429eac9acbd2eb8a747be8c7f40341681da6b197
Sebastián <sesva0204@gmail.com>
2017-09-07 18:39:42 -0500

    StackLayout, Grid, Code vs Xmal y Base Xaml

---
+++ DemoXaml/DemoXaml/GridLayoutExOne.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoXaml
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GridLayoutEj1 : ContentPage
+	{
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var numberButton = (sender as Button);
+            var number = numberButton.Text;
+
+            lblNumber.Text += number;
+            //throw new NotImplementedException();
+        }
+
+        void Dial_Clicked(object sender, System.EventArgs e)
+        {
+            //Debug.WriteLine("Dialing {0}...", lblNumber.Text);
+            lblNumber.Text = "";
+        }
+
+		public GridLayoutEj1 ()
+		{
+			InitializeComponent ();
+
+		}
+	}
+}
---
+++ DemoXaml/DemoXaml/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoXaml
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (Device.OS == TargetPlatform.Android)
+            {
+
+            }else if(Device.OS == TargetPlatform.iOS)
+            {
+
+            }
+
+            Padding = new OnPlatform<Thickness>
+            {
+                Android = new Thickness(0),
+                iOS = new Thickness(0,20,0,0)
+            };
+
+            /*
+            Content = new Button()
+            {
+                Text = "¡Hola Mundo!",
+                TextColor = Color.Aqua,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            */
+
+            
+            
+        }
+    }
+}
