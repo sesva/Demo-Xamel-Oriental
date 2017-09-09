c1349e8ad73e1035580a17c343f93c0e167aa285
Sebastián <sesva0204@gmail.com>
2017-09-08 23:28:12 -0500

    Learning Image
    Se agrego una imagen en sus diferentes densidades, y se trabajo con nuget llamodo xlabel para manejar imagenes circulares.

---
+++ DemoXaml/DemoXaml/ImageDemo.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoXaml
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImageDemo : ContentPage
+	{
+		public ImageDemo ()
+		{
+			InitializeComponent ();
+
+            //image.Source = ImageSource.FromResource("sesva.jpg");
+		}
+	}
+}
